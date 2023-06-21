260781df93e12e65ea033425f523f06ecaa357cd
VICTORIA ANTONOVA <antonovava@gmail.com>
2023-06-21 09:47:25 +0300

    New course added

---
+++ youtube_adv_python_course/lists.py
@@ -0,0 +1,9 @@
+# Lists: ordered, mutable, allows duplicate elements
+mylist = ['banana', 'cherry', 'apple']
+print(mylist)
+
+mylist2 = list()
+print(mylist2)
+
+mylist3 = [5, True, 'apple', 'apple']
+print(mylist3)
