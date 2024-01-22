47e31f351e92a4c9e3b31dfd4a9feb3e3650d394
VICTORIA ANTONOVA <antonovava@gmail.com>
2024-01-22 21:26:13 +0200

    finished

--- Course_Py_Adv/helper.py
+++ Course_Py_Adv/helper.py
@@ -1,37 +1,70 @@
+# import logging
+# # logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', 
+# #                     datefmt='%m/%d/%Y %H:%M:%S') # this is the default 
+# #             # format https://docs.python.org/3/library/logging.html#logging.basicConfig
+
+# # # now we can have assecc to 5 levels of logging
+# # # DEBUG, INFO, WARNING, ERROR, CRITICAL
+# # logging.debug("This is a debug message")
+# # logging.info("This is a info message")
+# # logging.warning("This is a warning message")
+# # logging.error("This is a error message")
+# # logging.critical("This is a critical message")
+
+# # and if I run this:
+
+# # WARNING:root:This is a warning message
+# # ERROR:root:This is a error message
+# # CRITICAL:root:This is a critical message
+
+# # not all the messages are printed, because the default level is WARNING
+# # we can change it by setting basic config
+
+# # add this line to the beginning of the file
+# # logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %H:%M:%S') 
+# # and after that:
+# # 01/18/2024 20:00:19 - root - DEBUG - This is a debug message
+# # 01/18/2024 20:00:19 - root - INFO - This is a info message
+# # 01/18/2024 20:00:19 - root - WARNING - This is a warning message
+# # 01/18/2024 20:00:19 - root - ERROR - This is a error message
+# # 01/18/2024 20:00:19 - root - CRITICAL - This is a critical message
+
+# # now if I want to log from different modules, then if's best practice not to use this rool logger
+# # and to create ur own logger
+# # comment all from above except the first line:
+# logger = logging.getLogger(__name__) # this will create a logger with the name of the module
+#     # __name__ - global variable that will be replaced with the name of the module
+# # logger.info("Hello from helper")
+# # and then in ur mail.py file
+# # --------------
+# logger.propagate = False # this will prevent the message to be printed twice
+# logger.info("Hello from helper") # this will print the message from helper.py
+
+# --------------
+# How to show different log handlers:
 import logging
-# logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', 
-#                     datefmt='%m/%d/%Y %H:%M:%S') # this is the default 
-#             # format https://docs.python.org/3/library/logging.html#logging.basicConfig
 
-# # now we can have assecc to 5 levels of logging
-# # DEBUG, INFO, WARNING, ERROR, CRITICAL
-# logging.debug("This is a debug message")
-# logging.info("This is a info message")
-# logging.warning("This is a warning message")
-# logging.error("This is a error message")
-# logging.critical("This is a critical message")
+logger = logging.getLogger(__name__) # __name__ - this will create a logger with the name of the module
 
-# and if I run this:
+# create my handler
+stream_h = logging.StreamHandler() # this will create a handler that will print to the console
+file_h = logging.FileHandler('file.log') # this will create a handler that will print to the file
 
-# WARNING:root:This is a warning message
-# ERROR:root:This is a error message
-# CRITICAL:root:This is a critical message
+# then for each handler u want to set level and the format
+stream_h.setLevel(logging.WARNING) # this will set the level of the handler
+file_h.setLevel(logging.ERROR) # this will set the level of the handler
 
-# not all the messages are printed, because the default level is WARNING
-# we can change it by setting basic config
+# now we also specify some formater, so we say
+formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s') # this will set the format of the handler
+stream_h.setFormatter(formatter) 
+file_h.setFormatter(formatter) 
 
-# add this line to the beginning of the file
-# logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %H:%M:%S') 
-# and after that:
-# 01/18/2024 20:00:19 - root - DEBUG - This is a debug message
-# 01/18/2024 20:00:19 - root - INFO - This is a info message
-# 01/18/2024 20:00:19 - root - WARNING - This is a warning message
-# 01/18/2024 20:00:19 - root - ERROR - This is a error message
-# 01/18/2024 20:00:19 - root - CRITICAL - This is a critical message
+# this will add the handler to the logger
+logger.addHandler(stream_h)
+logger.addHandler(file_h)
 
-# now if I want to log from different modules, then if's best practice not to use this rool logger
-# and to create ur own logger
-# comment all from above except the first line:
-logger = logging.getLogger(__name__) # this will create a logger with the name of the module
-logger.info("Hello from helper")
-# and then in ur mail.py file:
+logger.warning("This is a warning")
+logger.error("This is a error") # this will be printed to the file
+# __main__ - WARNING - This is a warning
+# __main__ - ERROR - This is a error
+
--- Course_Py_Adv/main.py
+++ Course_Py_Adv/main.py
@@ -1,4 +1,78 @@
-import logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', 
-                    datefmt='%m/%d/%Y %H:%M:%S')
-import helper
+# # https://www.youtube.com/watch?v=p0A4CV4MWd0&list=PLqnslRFeH2UqLwzS0AwKDKLrpYBKzLBy2&index=12
+# import logging
+# logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', 
+#                     datefmt='%m/%d/%Y %H:%M:%S')
+# import helper # this will print the message from helper.py
+# # 01/18/2024 20:00:19 - helper - INFO - Hello from helper
+# # but if I don't want to print the message from helper.py (propagation), then I can do this: (40,41)
+# # then nothing will be printed
+
+# # --------------
+# import logging
+# import logging.config
+
+# logging.config.fileConfig('logging.conf') # this will read the config file
+
+# logger = logging.getLogger('simpleExample') # this will create a logger with the name of the module
+# logger.debug('this is a debug message') # 2024-01-22 20:33:34,740 - simpleExample - DEBUG - this is a debug message
+
+# --------------
+# about capturing stack traces - could be helpful for debugging (trubleshooting)
+# for expample if u run the code witch raises an exception, then u can capture the stack trace
+# import logging
+
+# try:
+#     a = [1,2,3]
+#     val = a[4]
+# except IndexError as e:
+#     logging.error(e, exc_info=True) # this will print the stack trace
+#     # IndexError: list index out of range
+
+# # or we don't know what exception will be raised
+# import logging
+# import traceback
+
+# try:
+#     a = [1,2,3]
+#     val = a[4]
+# except:
+#     logging.error("the error is %s", traceback.format_exc()) # this will print the stack trace
+
+# # --------------
+# # let's talk about rotating files
+# # when u have a lot of logs, then u can rotate them
+# import logging
+# from logging.handlers import RotatingFileHandler
+
+# logger = logging.getLogger(__name__)
+# logger.setLevel(logging.INFO)
+
+# # roll over after 2KB, and keep backup logs app.log.1, app.log.2 , etc.
+# handler = RotatingFileHandler('app.log', maxBytes=2000, backupCount=5) # this will create a handler that will print to the file
+# logger.addHandler(handler)
+
+# for _ in range(10000):
+#     logger.info('Hello, world!') # this will print the message from helper.py
+
+# # --------------
+# # now let's talk about time rotating file handler
+# import logging
+# import time
+# from logging.handlers import TimedRotatingFileHandler
+# # and this will create a new file depending on how much time has passed
+# logger = logging.getLogger(__name__)
+# logger.setLevel(logging.INFO)
+
+# # s, m, h, d, midnight, w0-w6
+# handler = TimedRotatingFileHandler('timed_test.log', when='s', interval=5, backupCount=5) # this will create a handler that will print to the file
+# logger.addHandler(handler)
+
+# for _ in range(6):
+#     logger.info('Hello, world!')
+#     time.sleep(5)
+
+# --------------
+# if u have a lot of modules and a lot of logged things, 
+# especially if u use microservise architecture, then u can
+# use JSON format for logging
+# https://github.com/madzak/python-json-logger
