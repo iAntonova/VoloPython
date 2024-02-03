11a7fb79b6bf84ac9bf14f6695c2ab2873819d73
VICTORIA ANTONOVA <antonovava@gmail.com>
2024-02-03 16:14:28 +0200

    cleanup code

---
+++ Course_Py_Adv/trutest.py
@@ -0,0 +1,21 @@
+# create  a list of 100 numbers and find the max number
+
+my_list = [i for i in range(100)]
+print(my_list)
+
+
+def get_even_numbers(numbers):
+    even_numbers = []
+    for number in numbers:
+        if number % 2 == 0:
+            even_numbers.append(number)
+    return even_numbers
+
+
+even_numbers = get_even_numbers(numbers=my_list)
+
+print(even_numbers)  # [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48,
+# 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80, 82, 84, 86, 88, 90, 92, 94, 96, 98]
+
+odd_numbers =
+print(odd_numbers)  # [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49,
